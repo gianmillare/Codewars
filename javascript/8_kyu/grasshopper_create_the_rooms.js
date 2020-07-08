ecec74fd812ed875316efd8b83a0310a079e31f9
Gian Millare <gianmillaregsm@gmail.com>
2020-07-08 13:47:45 -0700

    js 8kyu += grasshopper create the rooms

---
+++ javascript/8_kyu/grasshopper_create_the_rooms.js
@@ -0,0 +1,20 @@
+// Grasshopper - Create the rooms
+// https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+var rooms = {
+  "room_1": {
+    "name": "red room",
+    "description": "a room painted red",
+    "completed": true
+  },
+  "room_2": {
+    "name": "green room",
+    "description": "a room painted green",
+    "completed": true
+  },
+  "room_3": {
+    "name": "red room",
+    "description": "a room painted blue",
+    "completed": false
+  }
+};
