79a5e49692bf8f21719eeedd2dd397319462f8da
gianmillare <gianmillaregsm@gmail.com>
2020-07-21 17:37:14 -0700

    fixed misplaced coded file, completed += 1 js challenge

---
+++ javascript/8_kyu/multiply.js
@@ -0,0 +1,7 @@
+// Multiply
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+
+function multiply(a, b) {
+    return a * b;
+  }
+  
--- python/8_kyu/multiply.py
+++ python/8_kyu/multiply.py
@@ -1,23 +1,33 @@
-cities = {
-  'san jose': {
-    'country': 'united states',
-    'population': 200000000,
-    'fact': 'has the best food'
-  },
-  'los angeles': {
-    'country': 'united states',
-    'population': 15000000000,
-    'fact': 'has the most actors'
-  },
-  'manila': {
-    'country': 'philippines',
-    'population': 2000,
-    'fact': 'is a third world country'
-  },
-}
+# Multiply
+# https://www.codewars.com/kata/50654ddff44f800200000004/train/python
 
-for x, y in cities.items():
-  country = y['country']
-  population = y['population']
-  fact = y['fact']
-  print(f"{x.title()} is a city in the {country.title()}. The city has a population of about {population} and {fact}!\n")
+def multiply(a, b):
+    return a * b
+
+
+
+
+# Misplaced code below -- please ignore --
+# cities = {
+#   'san jose': {
+#     'country': 'united states',
+#     'population': 200000000,
+#     'fact': 'has the best food'
+#   },
+#   'los angeles': {
+#     'country': 'united states',
+#     'population': 15000000000,
+#     'fact': 'has the most actors'
+#   },
+#   'manila': {
+#     'country': 'philippines',
+#     'population': 2000,
+#     'fact': 'is a third world country'
+#   },
+# }
+
+# for x, y in cities.items():
+#   country = y['country']
+#   population = y['population']
+#   fact = y['fact']
+#   print(f"{x.title()} is a city in the {country.title()}. The city has a population of about {population} and {fact}!\n")
