2e3c27dee4ed838b02cd19dd041ca1b496137a64
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2020-07-26 22:00:36 -0700

    js 8kyu += 1

---
+++ javascript/8_kyu/polish_alphabet.js
@@ -0,0 +1,30 @@
+// Polish alphabet
+// https://www.codewars.com/kata/57ab2d6072292dbf7c000039/train/javascript
+
+var polish_dict = {
+    'ą': 'a',
+    'ć': 'c',
+    'ę': 'e',
+    'ł': 'l',
+    'ń': 'n',
+    'ó': 'o',
+    'ś': 's',
+    'ź': 'z',
+    'ż': 'z'
+};
+
+function correctPolishLetters(s) {
+    var ans = [];
+
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] in polish_dict) {
+            ans.push(polish_dict[s[i]]);
+        } else {
+            ans.push(s[i]);
+        }
+    }
+
+    return ans.join('');
+}
+
+console.log(correctPolishLetters("Jędrzej Błądziński"));
