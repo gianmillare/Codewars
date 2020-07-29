e053f3ec8ccbeca1a14d3dd042ece76dcbdd6199
gianmillare <gianmillaregsm@gmail.com>
2020-07-28 20:41:41 -0700

    js 8kyu += watermelon

---
+++ javascript/8_kyu/watermelon.js
@@ -0,0 +1,10 @@
+// Watermelon
+// https://www.codewars.com/kata/55192f4ecd82ff826900089e/train/javascript
+
+function divide(weight){
+    if (weight > 2 && weight % 2 == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
