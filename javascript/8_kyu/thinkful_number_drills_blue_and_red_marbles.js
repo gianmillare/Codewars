1dcd3d76dce4f4a57c31be415512d7cd0e246a91
Gian Millare <gianmillaregsm@gmail.com>
2020-07-29 13:22:30 -0700

    js 8kyu += thinkful number drills blue and red

---
+++ javascript/8_kyu/thinkful_number_drills_blue_and_red_marbles.js
@@ -0,0 +1,6 @@
+// Thinkful - Number Drills: Blue and red marbles
+// https://www.codewars.com/kata/5862f663b4e9d6f12b00003b/train/javascript
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  return (blueStart - bluePulled) / ((blueStart - bluePulled) + (redStart - redPulled));
+}
