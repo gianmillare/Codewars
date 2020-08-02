e89ac0f56bcb77e8d1667538228341a3ae8db054
gianmillare <gianmillaregsm@gmail.com>
2020-08-01 21:01:29 -0700

    js 8kyu += beginner series 4 cockroach

---
+++ javascript/8_kyu/beginner_series_4_cockroach.js
@@ -0,0 +1,7 @@
+// Beginner Series #4 Cockroach
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+function cockroachSpeed(s) {
+    //   1 km/hr = 27.7778 cm/s
+      return Math.floor(s * 27.7778);
+    }
