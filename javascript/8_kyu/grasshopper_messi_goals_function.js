e7b3c7d5852478126b2ff4ba5e3731cc186bb9e5
gianmillare <gianmillaregsm@gmail.com>
2020-08-01 19:49:32 -0700

    js 8kyu += grasshopper messi goals

---
+++ javascript/8_kyu/grasshopper_messi_goals_function.js
@@ -0,0 +1,6 @@
+// Grasshopper - Messi goals function
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
