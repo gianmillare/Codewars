a95cf85b894c23ce86984af29882ffd83c4d0f9e
gianmillare <gianmillaregsm@gmail.com>
2020-08-03 12:14:27 -0700

    js 8kyu += volume of a cuboid

---
+++ javascript/8_kyu/volume_of_a_cuboid.js
@@ -0,0 +1,15 @@
+// Volume of a Cuboid
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
