1b4c2d4dfe7a5f09edcd73a01b61d2b3089c9d84
gianmillare <gianmillaregsm@gmail.com>
2020-08-03 12:46:37 -0700

    js 8kyu += reversed strings

---
+++ javascript/8_kyu/reversed_strings.js
@@ -0,0 +1,7 @@
+// Reversed Strings
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+function solution(str){
+    var ans = str.split('');
+    return ans.reverse().join('');
+  }
