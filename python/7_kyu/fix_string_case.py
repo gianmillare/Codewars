24c293c467398326a51e0b5a18ca7bf085d2aeb0
gianmillare <gianmillaregsm@gmail.com>
2020-08-06 21:28:30 -0700

    py 7kyu += fix string case

---
+++ python/7_kyu/fix_string_case.py
@@ -0,0 +1,29 @@
+# Fix string case
+# https://www.codewars.com/kata/5b180e9fedaa564a7000009a/solutions/python
+
+def solve(s):
+    uppercase = 0
+    lowercase = 0
+    
+    for i in s:
+        if i.isupper():
+            uppercase += 1
+        else:
+            lowercase += 1
+    
+    ans = []
+    
+    if lowercase >= uppercase:
+        for i in s:
+            if i.isupper():
+                ans.append(i.lower())
+            else:
+                ans.append(i)
+        return ''.join(ans)
+    elif lowercase < uppercase:
+        for i in s:
+            if i.islower():
+                ans.append(i.upper())
+            else:
+                ans.append(i)
+        return ''.join(ans)
