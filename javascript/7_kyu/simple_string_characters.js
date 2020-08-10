5e5dcb366639e93d4e88d18d6b64b1eac259e340
gianmillare <gianmillaregsm@gmail.com>
2020-08-10 11:34:32 -0700

    js 7kyu += simple string characters

---
+++ javascript/7_kyu/simple_string_characters.js
@@ -0,0 +1,26 @@
+// 
+// 
+
+function solve(s){
+    var u = 0;
+    var l = 0;
+    var n = 0;
+    var special = 0;
+    var letters = /^[A-Za-z]+$/;
+    
+    for (var i = 0; i < s.length; i++) {
+      if (s[i].match(letters) && s[i] == s[i].toUpperCase()) {
+          u++;
+      } else if (s[i].match(letters) && s[i] == s[i].toLowerCase()) {
+          l++;
+      } else if ( !(isNaN(s[i])) ) {
+          n++;
+      } else {
+          special++;
+      }
+    }
+
+    return [u, l, n, special];
+  }
+
+console.log(solve("Codewars@codewars123.com"));
