479a228f160bc25014c9259ce0f66877082d3376
Gian Millare <gianmillaregsm@gmail.com>
2020-08-19 12:17:49 -0700

    py 7kyu += nth power rules them all

---
+++ python/7_kyu/nth_power_rules_them_all.py
@@ -0,0 +1,7 @@
+# Nth power rules them all!
+# https://www.codewars.com/kata/58aed2cafab8faca1d000e20/train/python
+
+def modified_sum(a, n):
+      return sum([i**n for i in a]) - sum(a)
+
+print(modified_sum([1, 2, 3], 3))
