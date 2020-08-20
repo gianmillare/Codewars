17dc7b79bbfadf4c525fd028e9963f140d28d46f
Gian Millare <gianmillaregsm@gmail.com>
2020-08-20 08:22:46 -0700

    js 7kyu += split in parts

---
+++ javascript/7_kyu/split_in_parts.js
@@ -0,0 +1,15 @@
+// Split In Parts
+// https://www.codewars.com/kata/5650ab06d11d675371000003/train/javascript
+
+var splitInParts = function(s, partLength){
+  var ans = [];
+  var original_inc = partLength;
+  
+  for (var i = 0; i < s.length; i += partLength) {
+    ans.push(s.slice(i, i + partLength));
+  }
+  
+  return ans.join(' ')
+}
+
+console.log(splitInParts("supercalifragilisticexpialidocious", 3))
