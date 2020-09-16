38ba829126df0dc84abfb5e290dc8a1a639e9f83
Gian Millare <gianmillaregsm@gmail.com>
2020-09-16 14:38:54 -0700

    adding alternative solution to stop spinning my words py 6kyu

--- python/6_kyu/stop_spinning_my_words.py
+++ python/6_kyu/stop_spinning_my_words.py
@@ -12,6 +12,10 @@ def spin_words(sentence):
   
   return " ".join(ans)
 
+# # Alternative Solution: Python one-liner
+# def spin_words(sentence):
+#     return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+
 
 print(spin_words("Hey fellow warriors"))
 print(spin_words("This is another test"))
