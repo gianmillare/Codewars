0d9913a23aed76dcc0e4a8fe3ab186b5e165fbb5
gianmillare <gianmillaregsm@gmail.com>
2020-09-15 19:50:45 -0700

    adding alternative solution to JS 6kyu find the parity outlier

--- javascript/6_kyu/find_the_parity_outlier.js
+++ javascript/6_kyu/find_the_parity_outlier.js
@@ -20,6 +20,13 @@ function findOutlier(integers) {
     }
 }
 
+// Best Solution: Short syntax JS, filter
+// function findOutlier(int){
+//     var even = int.filter(a=>a%2==0);
+//     var odd = int.filter(a=>a%2!==0);
+//     return even.length==1? even[0] : odd[0];
+//   }
+
 console.log(findOutlier([2,6,8,10,3]));
 console.log(findOutlier([1, 2, 3]));
 console.log(findOutlier([1,1,0,1,1]));
