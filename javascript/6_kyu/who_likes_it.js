69e28c02035adb5800474d69b8f13bdf98080dad
Gian Millare <gianmillaregsm@gmail.com>
2020-09-17 08:56:32 -0700

    alternative solution to js 6kyu who likes it

--- javascript/6_kyu/who_likes_it.js
+++ javascript/6_kyu/who_likes_it.js
@@ -1,6 +1,7 @@
 // Who likes it?
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
 
+// Solution 1: Template Literals
 function likes(names) {
   if (names.length == 0) {
     return "no one likes this";
@@ -12,5 +13,16 @@ function likes(names) {
     return `${names[0]}, ${names[1]} and ${names[2]} like this`
   } else {
     return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+  }
+}
+
+// Solution 2: Switch Case method
+function likes(names) {
+  switch(names.length) {
+      case 0: return 'no one likes this'; break;
+      case 1: return `${names[0]} likes this`; break;
+      case 2: return `${names[0]} and ${names[1]} like this`; break;
+      case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`; break;
+      default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
 }
