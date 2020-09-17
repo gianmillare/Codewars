d0c3dc418f6a6f46d59bd4604c27a38f220f45ec
Gian Millare <gianmillaregsm@gmail.com>
2020-09-17 08:04:11 -0700

    alternative solution to py 6kyu who likes it using %s

--- python/6_kyu/who_likes_it.py
+++ python/6_kyu/who_likes_it.py
@@ -1,6 +1,7 @@
 # Who likes it?
 # https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
 
+# Solution 1: Using f-string
 def likes(names):
     if len(names) == 0:
         return "no one likes this"
@@ -12,3 +13,16 @@ def likes(names):
         return (f"{names[0]}, {names[1]} and {names[2]} like this")
     else:
         return (f"{names[0]}, {names[1]} and {len(names) - 2} others like this")
+
+# Solution 2: Using %
+def likes(names):
+    if len(names) == 0:
+        return "no one likes this"
+    elif len(names) == 1:
+        return "%s likes this" % names[0]
+    elif len(names) == 2:
+        return "%s and %s like this" % (names[0], names[1])
+    elif len(names) == 3:
+        return "%s, %s and %s like this" % (names[0], names[1], names[2])
+    else:
+        return "%s, %s and %s others like this" % (names[0], names[1], len(names) - 2)
