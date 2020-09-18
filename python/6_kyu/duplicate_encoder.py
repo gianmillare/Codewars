a25fb623fa733690e848836a96df52ccb3feffd5
Gian Millare <gianmillaregsm@gmail.com>
2020-09-18 08:52:12 -0700

    including most pythonic solution to 6kyu py duplicate encoder

--- python/6_kyu/duplicate_encoder.py
+++ python/6_kyu/duplicate_encoder.py
@@ -19,4 +19,8 @@ def duplicate_encode(word):
   word = word.lower()
   return ''.join([")" if word.count(i) > 1 else "(" for i in word])
 
+# Solution 3: Most Pythonic
+def duplicate_encode(word):
+    return "".join(["(" if word.lower().count(i) == 1 else ")" for i in word.lower()])
+
 print(duplicate_encode("Success"))
