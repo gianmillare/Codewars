9b3a166ec560fe36a9b35c29eacd375b04c030b6
gianmillare <gianmillaregsm@gmail.com>
2020-09-19 17:02:27 -0700

    adding switch case solution to js 6kyu take a ten minute walk

--- javascript/6_kyu/take_a_ten_minute_walk.js
+++ javascript/6_kyu/take_a_ten_minute_walk.js
@@ -1,6 +1,7 @@
 // Take a Ten Minute Walk
 // https://www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
 
+// Solution 1: Dictionary method
 function isValidWalk(walk) {
     if ( !(walk.length == 10) ) {
         return false;
@@ -21,6 +22,24 @@ function isValidWalk(walk) {
     }
 
     return true;
+}
+
+// Solution 2: Switch Case
+function isValidWalk(walk) {
+    var directionX = 0;
+    var directionY = 0;
+    var directionTime = walk.length;
+
+    for (var i = 0; i < directionTime; i++) {
+        switch (walk[i]) {
+            case 'n': directionY--; break;
+            case 's': directionY++; break;
+            case 'w': directionX--; break;
+            case 'e': directionX++; break; 
+        }
+    }
+
+    return directionTime === 10 && directionX === 0 && directionY === 0;
 }
 
 console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']));
