52b21acf49550e4615fba42c102e8744ca992e6c
Gian Millare <gianmillaregsm@gmail.com>
2020-09-21 15:46:30 -0700

    py 6kyu += iq test

---
+++ python/6_kyu/iq_test.py
@@ -0,0 +1,23 @@
+# IQ Test
+# https://www.codewars.com/kata/552c028c030765286c00007d/train/python
+
+# Solution 1: Map Function
+def iq_test(numbers):
+  nums =  list(map(int, numbers.split(" ")))
+  even_or_odd = {"even": [], "odd": []}
+  
+  for i in nums:
+    if i % 2 == 0:
+      even_or_odd['even'].append(i)
+    else:
+      even_or_odd['odd'].append(i)
+  
+  for key in even_or_odd:
+    if len(even_or_odd[key]) == 1:
+      for i in range(0, len(nums)):
+        if nums[i] == even_or_odd[key][0]:
+          return i + 1
+
+print(iq_test("2 4 7 8 10"))
+print(iq_test("1 2 1 1"))
+print(iq_test("1 2 2"))
