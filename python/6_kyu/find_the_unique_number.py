47a7cf3632b4dc4d81b213105c45584b3114866d
Gian Millare <gianmillaregsm@gmail.com>
2020-09-25 09:10:06 -0700

    py 6kyu += find the unique number

---
+++ python/6_kyu/find_the_unique_number.py
@@ -0,0 +1,10 @@
+# Find the unique number
+# https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/python
+
+# Solution 1: Pythonic For Loop
+def find_uniq(arr):
+  return [i for i in arr if arr.count(i) == 1][0]
+
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+print(find_uniq([ 0, 0, 0.55, 0, 0 ]))
+print(find_uniq([ 3, 10, 3, 3, 3 ])) 
