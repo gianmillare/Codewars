228d96f3270ecf4976172030e182772ea59c7ee9
gianmillare <gianmillaregsm@gmail.com>
2020-09-27 16:28:24 -0700

    pushing alternative solution to swift 8yku reversed strings

--- swift/8_kyu/reversed_strings.swift
+++ swift/8_kyu/reversed_strings.swift
@@ -1,8 +1,20 @@
 // Reversed Strings
 // https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/swift
 
+// Solution 1: Reverse functions
 func reverse(_ str: String) -> String {
     return String(str.reversed())
+}
+
+// Solution 2: For Loop Concatenation
+func reverse(_ str: String) -> String {
+    var ans = ""
+
+    for s in str {
+        ans = String(s) + ans
+    }
+
+    return ans
 }
 
 print(reverse("hello"))
