0dcfc8b07c5b2c522ff176a9b7c73e990f924d18
gianmillare <gianmillaregsm@gmail.com>
2020-09-27 18:29:05 -0700

    alternative solution to swift 8kyu convert number to reversed array of digits

--- swift/8_kyu/convert_number_to_reversed_array_of_digits.swift
+++ swift/8_kyu/convert_number_to_reversed_array_of_digits.swift
@@ -1,10 +1,16 @@
 // Convert number to reversed array of digits
 // https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/swift
 
+// Solution 1: compactMap and reversed function
 func digitize(_ num: Int) -> [Int] {
     let digits = String(num)
     let ans = digits.compactMap{ $0.wholeNumberValue }
     return ans.reversed()
+}
+
+// Solution 2: map + reversed
+func digitize(_ num: Int) -> [Int] {
+    return String(num).map{ $0.wholeNumberValue! }.reversed()
 }
 
 print(digitize(23582357))
