996640120c77b92301b70e054036a5d54e72c55e
gianmillare <gianmillaregsm@gmail.com>
2020-09-28 19:56:20 -0700

    swift 8kyu += opposite number

---
+++ swift/8_kyu/opposite_number.swift
@@ -0,0 +1,10 @@
+// Opposite number
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/swift
+
+func opposite(_ number: Int) -> Int {
+    return -number
+}
+
+print(opposite(1))
+print(opposite(14))
+print(opposite(-34))
