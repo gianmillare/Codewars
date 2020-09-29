195fe461d2babbc3189ed880e77d2734d553b439
Gian Millare <gianmillaregsm@gmail.com>
2020-09-29 09:05:13 -0700

    adding alternative solution to swift 8yku get planet name by id

--- swift/8_kyu/get_planet_name_by_id.swift
+++ swift/8_kyu/get_planet_name_by_id.swift
@@ -1,6 +1,7 @@
 // Get Planet Name By ID
 // https://www.codewars.com/kata/515e188a311df01cba000003/train/swift
 
+// Solution 1
 func getPlanetName(_ id: Int) -> String {
   var name: String
   switch id {
@@ -24,4 +25,19 @@ func getPlanetName(_ id: Int) -> String {
       name = ""
   } 
   return name
+}
+
+// Solution 2
+func getPlanetName(_ id: Int) -> String {
+  switch id {
+    case 1: return "Mercury"
+    case 2: return "Venus"
+    case 3: return "Earth"
+    case 4: return "Mars"
+    case 5: return "Jupiter"
+    case 6: return "Saturn"
+    case 7: return "Uranus"
+    case 8: return "Neptune"
+    default: return ""
+  } 
 }
