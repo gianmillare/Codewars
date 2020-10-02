752ca6b7819bfe5920cff220957711fde3060850
gianmillare <gianmillaregsm@gmail.com>
2020-10-01 19:23:53 -0700

    adding alternative solution to py 6kyu bit counting. Easier to read solution.

--- python/6_kyu/bit_counting.py
+++ python/6_kyu/bit_counting.py
@@ -1,10 +1,18 @@
 # Bit Counting
 # https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
 
+# Solution 1: Pythonic
 def count_bits(n):
     bit_number = sum(list(map(int, list(str(bin(n)))[2:])))
     return bit_number
-    
+
+# Solution 2: Easier to read (Pythonic)
+def count_bits(n):
+    bit_form = bin(n)
+    bit_string = str(bit_form)
+    bit_list = list(bit_string[2:])
+    bit_list_as_integers = list(map(int, bit_list))
+    return sum(bit_list_as_integers)
     
 print(count_bits(1234))
 print(count_bits(0))
