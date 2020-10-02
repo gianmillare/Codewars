fb72a325511c544953a195618b04c20ec206c1e3
Gian Millare <gianmillaregsm@gmail.com>
2020-10-02 15:33:14 -0700

    adding alternate solution to py 6kyu convert string to camel case

--- python/6_kyu/convert_string_to_camel_case.py
+++ python/6_kyu/convert_string_to_camel_case.py
@@ -1,9 +1,9 @@
 # Convert String to Camel Case
 # https://www.codewars.com/kata/517abf86da9663f1d2000003/train/python
 
-
 import re
 
+# Solution 1: re.split
 def to_camel_case(text):
   if "-" in text or "_" in text:
     text = re.split('-|_', text)
@@ -16,6 +16,18 @@ def to_camel_case(text):
     ans.append(text[i][0].upper() + text[i][1:])
   
   return "".join(ans)
+
+# Solution 2: replace
+def to_camel_case(text):
+  text = text.replace("_", "-").split("-")
+  
+  if len(text) == 0:
+    return ""
+  
+  return text[0] + "".join([i.capitalize() for i in text[1:]])
+
+
+
 print(to_camel_case(''))
 print(to_camel_case('the_stealth_warrior'))
 print(to_camel_case('The-Stealth-Warrior'))
