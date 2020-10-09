28051a80b3b7f4d4d7f671884cdff4e7b0cb00ed
gianmillare <gianmillaregsm@gmail.com>
2020-10-08 19:50:18 -0700

    py 6kyu += build tower

---
+++ python/6_kyu/build_tower.py
@@ -0,0 +1,21 @@
+# Build Tower
+# https://www.codewars.com/kata/576757b1df89ecf5bd00073b/train/python
+
+def tower_builder(n):
+    if n == 1:
+        return ['*']
+    else:
+        length = n + (n - 1)
+        ans = []
+
+        for star in range(1, length + 1, 2):
+            spaces = length - star
+            sep = ' ' * int(spaces / 2)
+            stars = '*' * star
+            ans.append(sep + stars + sep)
+
+        return ans
+
+print(tower_builder(1))
+print(tower_builder(2))
+print(tower_builder(3))
