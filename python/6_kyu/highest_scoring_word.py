0aa7850e0575647963ce779dbdd1cbc021c142de
gianmillare <gianmillaregsm@gmail.com>
2020-10-09 19:22:47 -0700

    py 6kyu += highest scoring word

---
+++ python/6_kyu/highest_scoring_word.py
@@ -0,0 +1,19 @@
+# Highest Scoring Word
+# https://www.codewars.com/kata/57eb8fcdf670e99d9b000272/train/python
+
+def make_scorecard():
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    point, points = 1, {}
+
+    for i in alpha:
+        points[i] = point
+        point += 1
+    
+    return points
+
+def high(x):
+    return x
+
+print(high("man i need a taxi up to ubud"))
+print(high("what time are we climbing up the volcano"))
+print(high("take me to semynak"))
