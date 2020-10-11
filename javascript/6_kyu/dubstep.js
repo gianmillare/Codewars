d32d0681c0a2a17177ef917ba508e140d57ff3e4
gianmillare <gianmillaregsm@gmail.com>
2020-10-11 15:03:16 -0700

    alternative solution to js 6kyu dubstep

--- javascript/6_kyu/dubstep.js
+++ javascript/6_kyu/dubstep.js
@@ -1,12 +1,18 @@
 // Dubstep
 // https://www.codewars.com/kata/551dc350bf4e526099000ae5/train/javascript
 
+// Solution 1: Split, Filter, Join
 function songDecoder(s) {
     s = s.split("WUB");
 
     filteredSong = s.filter(function (x) {return x != ""});
 
     return filteredSong.join(" ");
+}
+
+// Solution 2: Split, Filter boolean, join
+function songDecoder(s) {
+    return s.split("WUB").filter(Boolean).join(" ");
 }
 
 console.log(songDecoder("AWUBBWUBC"));
