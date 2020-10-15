81ceb50c79012a231cff1e7e0a9c85e2394bffd7
gianmillare <gianmillaregsm@gmail.com>
2020-10-14 22:49:32 -0700

    py 5kyu completed moving zeros to the end

--- python/5_kyu/moving_zeros_to_the_end.py
+++ python/5_kyu/moving_zeros_to_the_end.py
@@ -1,12 +1,21 @@
 # Moving Zeros To The End
 # https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
 
+# Solution 1: isinstance and appending two lists
 def move_zeros(arr):
-  re_formatted = [int(i) if isinstance(i, float) else i for i in arr]
-  non_zero = [i for i in re_formatted if isinstance(i, str) or str(i) != "0"]
-  zero = [i for i in re_formatted if isinstance(i, int) and i == 0]
+  reformatted = [int(i) if isinstance(i, float) else i for i in arr]
+  ans, count = [], 0
+  
+  for i in reformatted:
+    if isinstance(i, str) or str(i) != "0":
+      ans.append(i)
+    elif i == 0 or i == "0":
+      count += 1
+  
+  for zero in range(count):
+    ans.append(0)
 
-  return non_zero + zero
+  return ans
 
 print(move_zeros([1,2,0,1,0,1,0,3,0,1]))
 print(move_zeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9]))
