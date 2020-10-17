b3cac659352e277c39542b3a10a0085798744a8f
gianmillare <gianmillaregsm@gmail.com>
2020-10-17 08:41:04 -0700

    py 5kyu += maximum subarray sum

---
+++ python/5_kyu/maximum_subarray_sum.py
@@ -0,0 +1,34 @@
+# Maximum Subarray Sum
+# https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/python
+
+# Solution 1: Find contingencies, Sub-Arrays Dictionary
+def locate_positive(arr):
+    positive_number_found = False
+    for i in arr:
+        if i > 0:
+            positive_number_found = True
+    
+    return positive_number_found
+
+def max_sequence(arr):
+    # check if the array is empty
+    if len(arr) == 0:
+        return 0
+
+    # check if all values are negative
+    if locate_positive(arr) == False:
+        return 0
+    
+    # create a dictionary to hold all the sub-arrays
+    sub_arrays = {}
+
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+           sub_arrays[sum(arr[i : j + 1])] = arr[i : j + 1]
+
+    sub_max = max(sub_arrays, key=int)
+    return sub_max
+
+# print(max_sequence([]))
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+# print(max_sequence([-3, -2, -1]))
