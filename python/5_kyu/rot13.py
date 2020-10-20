42d3c5f08e371885656b8767d8313930d497477e
gianmillare <gianmillaregsm@gmail.com>
2020-10-19 20:17:05 -0700

    py 5kyu += rot13

---
+++ python/5_kyu/rot13.py
@@ -0,0 +1,62 @@
+# Rot13
+# https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+
+# Solution 1: Iterating through dictionary + 
+# Create the cypher where each letter is given an integer
+def encode(arr):
+    cypher, count = {}, 0
+
+    for i in arr:
+        cypher[count] = i
+        count += 1
+    
+    return cypher
+
+# Create searching function to search for encrypted letter
+def search_cypher(x, cypher):
+    found = False
+
+    for key, value in cypher.items():
+        if value == x:
+            found = True
+    
+    return found
+
+# Encrypt the message using rot13 function
+def rot13(message):
+    # cypher the alphabet into a dictionary
+    cypher_lowecase = encode("abcdefghijklmnopqrstuvwxyz")
+    cypher_uppercase = encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    # Create an empty array to append into
+    res = []
+
+    # Iterate through the message, adding 13 to the encrypt character
+    for i in list(message):
+        if search_cypher(i, cypher_lowecase):
+            for key, value in cypher_lowecase.items():
+                if value == i:
+                    encrypted_char = key
+                    if encrypted_char + 13 >= 26:
+                        encrypted13 = (encrypted_char + 13) - 26
+                        res.append(cypher_lowecase[encrypted13])
+                    else:
+                        encrypted13 = encrypted_char + 13
+                        res.append(cypher_lowecase[encrypted13])
+        elif search_cypher(i, cypher_uppercase):
+            for key, value in cypher_uppercase.items():
+                if value == i:
+                    encrypted_char = key
+                    if encrypted_char + 13 >= 26:
+                        encrypted13 = (encrypted_char + 13) - 26
+                        res.append(cypher_uppercase[encrypted13])
+                    else:
+                        encrypted13 = encrypted_char + 13
+                        res.append(cypher_uppercase[encrypted13])
+        else:
+            res.append(i)
+        
+    return "".join(res)
+
+print(rot13("test"))
+print(rot13("Test"))
