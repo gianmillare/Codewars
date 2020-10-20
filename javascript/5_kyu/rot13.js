15afcf3017f6c1f2a903246dabaa2c8cc9971b6d
gianmillare <gianmillaregsm@gmail.com>
2020-10-19 20:41:33 -0700

    js 5kyu completed encoding and isAlpha functions. continuing rot13 message

---
+++ javascript/5_kyu/rot13.js
@@ -0,0 +1,36 @@
+// Rot13
+// https://www.codewars.com/kata/530e15517bc88ac656000716/train/python
+
+// Solution 1: Hard Coding dictionary and Encryption
+// Create function to make cypher dictionary
+function encode(arr){
+    var cypher = {}
+    var count = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        cypher[count] = arr[i];
+        count++;
+    }
+
+    return cypher;
+}
+
+// Create function to check if character is alphabetic
+function isAlpha(x) {
+    return /[A-Za-z]/.test(x);
+}
+
+function rot13(message) {
+    var cypherLowercase = encode("abcdefghijklmnopqrstuvwxyz");
+    var cypherUppercase = encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    message = message.split("");
+
+    for (var i = 0; i < message.length; i++) {
+        if ( isAlpha(message[i]) && message[i] == message[i].toUpperCase() ) {
+            for ( )
+        }
+    }
+}
+
+console.log(rot13("test"));
+console.log(rot13("Test"));
