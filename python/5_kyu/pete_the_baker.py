b33339788acec38ac3b772135bc286d0fda79eec
gianmillare <gianmillaregsm@gmail.com>
2020-10-20 20:56:00 -0700

    started 5 kyu pete the baker

---
+++ python/5_kyu/pete_the_baker.py
@@ -0,0 +1,8 @@
+# Pete, The Baker
+# https://www.codewars.com/kata/525c65e51bf619685c000059/train/python
+
+def cakes(recipe, available):
+    return recipe, available
+
+print(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}))
+print(cakes({"apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100}, {"sugar": 500, "flour": 2000, "milk": 2000}))
