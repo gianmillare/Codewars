1a061e172a83ea9dc99bd74e8135495ff6b2ce42
gianmillare <gianmillaregsm@gmail.com>
2020-10-21 12:53:31 -0700

    js 5kyu completed pete the baker

--- javascript/5_kyu/pete_the_baker.js
+++ javascript/5_kyu/pete_the_baker.js
@@ -1,13 +1,14 @@
 // Pete, The Baker
 // https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
+// Solution 1: Create dictionary and find Min
 function cakes(recipe, available) {
     var cakesMade = {};
 
     for ( const [key, value] of Object.entries(recipe) ) {
         if ( key in  available ) {
             cakesMade[key] = 0;
-            if ( recipe[key] <= available[key] ) {
+            while ( recipe[key] <= available[key] ) {
                 available[key] = available[key] - recipe[key];
                 cakesMade[key]++;
             }
@@ -16,7 +17,15 @@ function cakes(recipe, available) {
         }
     }
 
-    
+    var minimum_cakes = cakesMade[Object.keys(cakesMade)[0]];
+
+    for ( const [key, value] of Object.entries(cakesMade) ) {
+        if ( cakesMade[key] < minimum_cakes ) {
+            minimum_cakes = cakesMade[key];
+        }
+    }
+
+    return minimum_cakes;
 }
 
 console.log(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}));
