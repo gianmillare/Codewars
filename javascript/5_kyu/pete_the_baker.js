812c32817770efd95d278aee385130aedab714d2
gianmillare <gianmillaregsm@gmail.com>
2020-10-21 12:47:40 -0700

    progression with js 5kyu pete the baker, created dictionary and incrementing dictionary entries

--- javascript/5_kyu/pete_the_baker.js
+++ javascript/5_kyu/pete_the_baker.js
@@ -2,7 +2,21 @@
 // https://www.codewars.com/kata/525c65e51bf619685c000059/train/javascript
 
 function cakes(recipe, available) {
+    var cakesMade = {};
 
+    for ( const [key, value] of Object.entries(recipe) ) {
+        if ( key in  available ) {
+            cakesMade[key] = 0;
+            if ( recipe[key] <= available[key] ) {
+                available[key] = available[key] - recipe[key];
+                cakesMade[key]++;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    
 }
 
 console.log(cakes({"flour": 500, "sugar": 200, "eggs": 1}, {"flour": 1200, "sugar": 1200, "eggs": 5, "milk": 200}));
