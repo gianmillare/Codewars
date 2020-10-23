61df73c344a112bc6a540d4b61736f6a00de4748
gianmillare <gianmillaregsm@gmail.com>
2020-10-22 20:12:26 -0700

    progression with py 5kyu weight for weight

--- python/5_kyu/weight_for_weight.py
+++ python/5_kyu/weight_for_weight.py
@@ -2,26 +2,17 @@
 # https://www.codewars.com/kata/55c6126177c9441a570000cc/train/python
 
 def order_weight(list_of_weights):
+  # split the list into a list of integers
   list_of_weights = [i for i in list_of_weights.split(" ")]
-  weighted = {}
-  
+  summed_weights = []
+
   for i in list_of_weights:
     digits = [int(j) for j in list(i)]
-    position = sum(digits)
-    weighted[position] = i
+    sum_digits = sum(digits)
 
-  weighted_sorted_keys = sorted(weighted)
-
-  res = []
-
-  for i in weighted_sorted_keys:
-    res.append(weighted[i])
+    summed_weights.append(sum_digits)
   
-  return " ".join(res)
-
-
-
-    
+  return summed_weights
 
 print(order_weight("103 123 4444 99 2000"))
 print( order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"))
