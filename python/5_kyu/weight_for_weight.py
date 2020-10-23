854c7700945abc6e5cb3966426b455fd653749b9
gianmillare <gianmillaregsm@gmail.com>
2020-10-22 21:25:47 -0700

    trying new solution for py 5kyu weights for weights

--- python/5_kyu/weight_for_weight.py
+++ python/5_kyu/weight_for_weight.py
@@ -1,18 +1,23 @@
 # Weight for Weight
 # https://www.codewars.com/kata/55c6126177c9441a570000cc/train/python
 
-def order_weight(list_of_weights):
-  # split the list into a list of integers
-  list_of_weights = [i for i in list_of_weights.split(" ")]
-  summed_weights = []
+def sum_digits(s):
+    return sum([int(i) for i in s])
 
-  for i in list_of_weights:
-    digits = [int(j) for j in list(i)]
-    sum_digits = sum(digits)
+def order_weights(string_of_weights):
+    original_list = string_of_weights.split()
+    weights = {}
+    
+    for i in original_list:
+        digits = sum_digits([j for j in i])
+        
+        if i in weights:
+            weights[i].append(digits)
+        else:
+            weights[i] = [digits]
+        
+    return weights
 
-    summed_weights.append(sum_digits)
-  
-  return summed_weights
 
-print(order_weight("103 123 4444 99 2000"))
-print( order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"))
+print(order_weights("103 123 4444 99 2000"))
+print(order_weights("2000 10003 1234000 44444444 9999 11 11 22 123"))
