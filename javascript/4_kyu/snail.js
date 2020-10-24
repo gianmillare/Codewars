548c98e94daa9f86bc595aee89fc3f985efd49ac
gianmillare <gianmillaregsm@gmail.com>
2020-10-23 22:58:11 -0700

    started js 4kyu snail

---
+++ javascript/4_kyu/snail.js
@@ -0,0 +1,9 @@
+// Snail
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+
+function snail(array) {
+    return array;
+}
+
+console.log(snail([[1,2,3],[4,5,6],[7,8,9]]));
+console.log(snail([[1,2,3],[8,9,4],[7,6,5]]));
