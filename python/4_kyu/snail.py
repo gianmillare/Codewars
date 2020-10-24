9be1d3ac4e4ecdd2c871ffb77bcf54a7dd0ad4d4
gianmillare <gianmillaregsm@gmail.com>
2020-10-24 09:09:04 -0700

    progress in py 4kyu snail. completed top and right side of n x n arrays

--- python/4_kyu/snail.py
+++ python/4_kyu/snail.py
@@ -2,7 +2,15 @@
 # https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/python
 
 def snail(snail_map):
-    return snail_map
+    # we know the first array in snail_map will always be first in the answer (top)
+    ans = [i for i in snail_map[0]]
+
+    # Loop through the rest of the array, removing the last values of each array (right)
+    for i in range(1, len(snail_map)):
+        ans.append(snail_map[i][-1])
+    
+    return ans
+    
 
 print(snail([[1,2,3],[4,5,6],[7,8,9]]))
 print(snail([[1,2,3],[8,9,4],[7,6,5]]))
