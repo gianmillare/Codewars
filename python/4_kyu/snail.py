bf05b9e43ec4ed560f9101d49fa2df50c1cb10fa
gianmillare <gianmillaregsm@gmail.com>
2020-10-23 22:55:42 -0700

    started py 4kyu snail

---
+++ python/4_kyu/snail.py
@@ -0,0 +1,8 @@
+# Snail
+# https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/python
+
+def snail(snail_map):
+    return snail_map
+
+print(snail([[1,2,3],[4,5,6],[7,8,9]]))
+print(snail([[1,2,3],[8,9,4],[7,6,5]]))
