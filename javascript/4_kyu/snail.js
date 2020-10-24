9e383dd1909f6e4ef98739bb5838a4e37c002496
gianmillare <gianmillaregsm@gmail.com>
2020-10-24 08:55:53 -0700

    adding new practice problem to js 4kyu snail

--- javascript/4_kyu/snail.js
+++ javascript/4_kyu/snail.js
@@ -7,3 +7,4 @@ function snail(array) {
 
 console.log(snail([[1,2,3],[4,5,6],[7,8,9]]));
 console.log(snail([[1,2,3],[8,9,4],[7,6,5]]));
+console.log(snail([[1,2,3,1],[4, 5, 6, 4],[7,8,9,7], [7,8,9,7]]));
