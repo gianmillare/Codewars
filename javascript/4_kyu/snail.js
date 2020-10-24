ed8cca16741f5c131b6a772c5ef2e6516851afc2
gianmillare <gianmillaregsm@gmail.com>
2020-10-24 09:17:00 -0700

    progress py 4kyu restructure and completed bottom array

--- javascript/4_kyu/snail.js
+++ javascript/4_kyu/snail.js
@@ -5,6 +5,15 @@ function snail(array) {
     return array;
 }
 
-console.log(snail([[1,2,3],[4,5,6],[7,8,9]]));
-console.log(snail([[1,2,3],[8,9,4],[7,6,5]]));
-console.log(snail([[1,2,3,1],[4, 5, 6, 4],[7,8,9,7], [7,8,9,7]]));
+console.log(snail([[1,2,3],
+    [4,5,6],
+    [7,8,9]]));
+
+console.log(snail([[1,2,3],
+    [8,9,4],
+    [7,6,5]]));
+    
+console.log(snail([[1,2,3,1],
+    [4,5,6,4],
+    [7,8,9,7], 
+    [7,8,9,7]]));
--- python/4_kyu/snail.py
+++ python/4_kyu/snail.py
@@ -8,10 +8,23 @@ def snail(snail_map):
     # Loop through the rest of the array, removing the last values of each array (right)
     for i in range(1, len(snail_map)):
         ans.append(snail_map[i][-1])
+
+    # Loop through the last array and append digits in reverse chronological order (bottom)
+    for i in range(len(snail_map[-1]) - 2, -1, -1):
+        ans.append(snail_map[-1][i])
     
     return ans
     
 
-print(snail([[1,2,3],[4,5,6],[7,8,9]]))
-print(snail([[1,2,3],[8,9,4],[7,6,5]]))
-print(snail([[1,2,3,1],[4, 5, 6, 4],[7,8,9,7], [7,8,9,7]]))
+print(snail([[1,2,3],
+             [4,5,6],
+             [7,8,9]]))
+
+print(snail([[1,2,3],
+             [8,9,4],
+             [7,6,5]]))
+             
+print(snail([[1,2,3,1],
+             [4,5,6,4],
+             [7,8,9,7], 
+             [7,8,9,7]]))
