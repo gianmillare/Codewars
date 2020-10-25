9f068f5800db3f3b4ede515cf6267801726eef02
gianmillare <gianmillaregsm@gmail.com>
2020-10-25 10:41:44 -0700

    completed py 4kyu snail. Solution working on IDE but few kinks in codewars solution. Will fix.

--- python/4_kyu/snail.py
+++ python/4_kyu/snail.py
@@ -5,26 +5,33 @@ res = []
 
 def snail(snail_map):
     # we know the first array in snail_map will always be first in the answer (top)
-    ans = [i for i in snail_map[0]]
-    snail_map.pop(0)
+    if snail_map:
+        ans = [i for i in snail_map[0]]
+        snail_map.pop(0)
 
     # Loop through the rest of the array, removing the last values of each array (right)
-    for i in range(len(snail_map)):
-        remove_value_to_append = snail_map[i].pop(-1)
-        ans.append(remove_value_to_append)
+    if snail_map:
+        for i in range(len(snail_map)):
+            remove_value_to_append = snail_map[i].pop(-1)
+            ans.append(remove_value_to_append)
 
     # Loop through the last array and append digits in reverse chronological order (bottom)
-    for i in range(len(snail_map[-1]) - 1, -1, -1):
-        remove_value_to_append = snail_map[-1].pop(i)
-        ans.append(remove_value_to_append)
+    if snail_map:
+        bottom_array = snail_map[-1]
+        
+        for i in range(len(bottom_array) - 1, -1, -1):
+            remove_value_to_append = snail_map[-1].pop(i)
+            ans.append(remove_value_to_append)
     
     # Remove the empty arrays
-    snail_map.remove([])
+    if snail_map:
+        snail_map.remove([])
 
     # Loop through the  1 value of each list excluding the first and last arrays
-    for i in range(len(snail_map) - 1, -1, -1):
-        remove_value_to_append = snail_map[i].pop(0)
-        ans.append(remove_value_to_append)
+    if snail_map:
+        for i in range(len(snail_map) - 1, -1, -1):
+            remove_value_to_append = snail_map[i].pop(0)
+            ans.append(remove_value_to_append)
     
     res.append(ans)
     
@@ -38,15 +45,15 @@ def snail(snail_map):
         
         return completed_snail_trail
 
-print(snail([[1,2,3],
-             [4,5,6],
-             [7,8,9]]))
+# print(snail([[1,2,3],
+#              [4,5,6],
+#              [7,8,9]]))
 
 # print(snail([[1,2,3],
 #              [8,9,4],
 #              [7,6,5]]))
 
-# print(snail([[1,2,3,1],
-#              [4,5,6,4],
-#              [7,8,9,7], 
-#              [7,8,9,7]]))
+print(snail([[1,2,3,1],
+             [4,5,6,4],
+             [7,8,9,7], 
+             [7,8,9,7]]))
