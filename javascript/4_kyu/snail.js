98c01c0e7a0fea0d457131bb8fb5003f4124d386
gianmillare <gianmillaregsm@gmail.com>
2020-10-26 19:05:47 -0700

    completed the right side of the snail trail for js 4kyu snail

--- javascript/4_kyu/snail.js
+++ javascript/4_kyu/snail.js
@@ -13,7 +13,22 @@ function snail(array) {
         for (var i = 0; i < array[0].length; i++) {
             ans.push(array[0][i]);
         }
+        // remove the values that were pushed into ans
         array.splice(0, 1);
+    }
+
+    // loop through the array and push values into ans equal to the last value of each sub-array
+    if (array) {
+        for (var i = 0; i < array.length; i++) {
+            // get the metrics of the last value of the sub-array --> length of subarray - 1
+            var array_length = array[i].length;
+            // Assign the last value to a variable
+            var value_to_push = array[i][array_length - 1];
+            // push the value into ans
+            ans.push(value_to_push);
+            // remove the value that was pushed into ans
+            array[i].splice(array_length - 1, 1);
+        }
     }
 
     console.log(array);
@@ -21,8 +36,8 @@ function snail(array) {
 }
 
 console.log(snail([[1,2,3],
-    [4,5,6],
-    [7,8,9]]));
+                   [4,5,6],
+                   [7,8,9]]));
 
 // console.log(snail([[1,2,3],
 //     [8,9,4],
