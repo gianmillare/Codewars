de5fd9ab8f4c5c3d67b4d0cd5cfa5cbc9b76aa60
gianmillare <gianmillaregsm@gmail.com>
2020-10-27 20:51:58 -0700

    started js 4kyu human readable duration format

---
+++ javascript/4_kyu/human_readable_duration_format.js
@@ -0,0 +1,12 @@
+// Human Readable Duration Format
+// https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
+
+function formatDuration(time) {
+    return time;
+}
+
+console.log(format_duration(1));
+console.log(format_duration(62));
+console.log(format_duration(120));
+console.log(format_duration(3600));
+console.log(format_duration(3662));
