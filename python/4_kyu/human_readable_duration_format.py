58bedf1f1ab75f529c267cf0a5484a73b2d0aa05
gianmillare <gianmillaregsm@gmail.com>
2020-10-27 20:49:10 -0700

    started py 4kyu human readable duration format

---
+++ python/4_kyu/human_readable_duration_format.py
@@ -0,0 +1,11 @@
+# Human Readable Duration Format
+# https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
+
+def format_duration(time):
+    return time
+
+print(format_duration(1))
+print(format_duration(62))
+print(format_duration(120))
+print(format_duration(3600))
+print(format_duration(3662))
