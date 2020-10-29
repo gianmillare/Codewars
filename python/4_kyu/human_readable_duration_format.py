f1be9d85844bb94e6254555455d97e8704b19b03
gianmillare <gianmillaregsm@gmail.com>
2020-10-28 23:33:07 -0700

    pushing the idea of an optional solution using division and modulus to determine the human readable format

--- python/4_kyu/human_readable_duration_format.py
+++ python/4_kyu/human_readable_duration_format.py
@@ -1,16 +1,31 @@
 # Human Readable Duration Format
 # https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
 
+# def format_duration(time):
+#     # if the time is equal to zero, return now
+#     if time == 0:
+#         return "now"
+    
+#     # if the time is less than 60 seconds, return time as a string and concatenate the word "seconds"
+#     if time == 1:
+#         return str(time) + " second"
+#     elif 1 < time < 60:
+#         return str(time) + " seconds" 
+
+# Another solution: Using division
 def format_duration(time):
-    # if the time is equal to zero, return now
     if time == 0:
         return "now"
     
-    # if the time is less than 60 seconds, return time as a string and concatenate the word "seconds"
     if time == 1:
         return str(time) + " second"
-    elif 1 < time < 60:
-        return str(time) + " seconds" 
+    
+    if 1 < time < 60:
+        return str(time) + " seconds"
+    
+    if time >= 60:
+        min = time / 60
+        seconds = time % 60
     
 
 
