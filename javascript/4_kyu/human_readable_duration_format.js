00c4e32c52f00f821dcc66f0719d193090253c41
gianmillare <gianmillaregsm@gmail.com>
2020-10-30 21:30:31 -0700

    progress with js 4kyu readable duration format. Created global variables and the formatted dictionary

--- javascript/4_kyu/human_readable_duration_format.js
+++ javascript/4_kyu/human_readable_duration_format.js
@@ -1,8 +1,24 @@
 // Human Readable Duration Format
 // https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/javascript
 
+// Declare the global variables
+const yearToSec = 31536000;
+const dayToSec = 86400;
+const hourToSec = 3600;
+const minToSec = 60;
+
+// Main Function
 function formatDuration(time) {
-    return time;
+    // Create the dictionary that will hold the amount needed in each duration
+    var format = {
+        "year": 0,
+        "day": 0,
+        "hour": 0,
+        "minute": 0,
+        "second": 0
+    };
+
+    
 }
 
 console.log(format_duration(1));
