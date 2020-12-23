d375e25acb84b85949d6000a9262ed461eeea5ed
gianmillare <gianmillaregsm@gmail.com>
2020-12-22 18:36:33 -0800

    completed even_or_odd.py in python 8kyu

---
+++ python/8_kyu/even_or_odd.py
@@ -0,0 +1,8 @@
+# Even or Odd
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/python
+
+def even_or_odd(n):
+    return "Even" if n % 2 == 0 else "Odd"
+
+print(even_or_odd(2))
+print(even_or_odd(7))
