6f5faa4c9d92ba1bd8f19687e67455772fc7562f
gianmillare <gianmillaregsm@gmail.com>
2020-12-23 17:36:08 -0800

    completed get_the_middle_character to python 7kyu

---
+++ python/7_kyu/get_the_middle_character.py
@@ -0,0 +1,22 @@
+# Get the Middle Character
+# https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
+
+def get_middle(s):
+    # check if the length of the word is even or odd
+    even_or_odd = "Even" if len(s) % 2 == 0 else "Odd"
+    
+    # Create a empyt list variable
+    ans = []
+
+    # conditional statement depending on even or odd
+    if even_or_odd == "Even":
+        ans.append(s[(len(s) / 2) - 1])
+        ans.append(s[len(s) / 2])
+    else:
+        ans.append(s[(len(s) / 2)])
+    
+    # join the list ans
+    return "".join(ans)
+
+print(get_middle("test"))
+print(get_middle("testing"))
