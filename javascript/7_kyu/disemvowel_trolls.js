92f4f21afc8074a6c68b35744c2aa3b5acb0bba5
gianmillare <gianmillaregsm@gmail.com>
2020-12-24 17:58:32 -0800

    completed js disemvowel_trolls on 7kyu

--- javascript/7_kyu/disemvowel_trolls.js
+++ javascript/7_kyu/disemvowel_trolls.js
@@ -1,3 +1,18 @@
 // Disemvowel Trolls
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/train/python
 
+const vowels = "aeiouAEIOU";
+
+function disemvowel(s) {
+    s = s.split('');
+
+    var ans = [];
+
+    for (i = 0; i < s.length; i++) {
+        if ( !(vowels.includes(s[i])) ) {
+            ans.push(s[i]);
+        }
+    }
+
+    return ans.join("");
+}
