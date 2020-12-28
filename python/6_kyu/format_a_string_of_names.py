5879471ec007a67607e90dc24d24a7252c8ef04b
gianmillare <gianmillaregsm@gmail.com>
2020-12-27 18:22:22 -0800

    python 6kyu format a string of names, completed contingency cases

--- python/6_kyu/format_a_string_of_names.py
+++ python/6_kyu/format_a_string_of_names.py
@@ -2,10 +2,28 @@
 # https://www.codewars.com/kata/53368a47e38700bd8300030d/train/python
 
 def namelist(names):
-    return names
+    # if the list is empty, return an empty string
+    if len(names) == 0:
+        return ""
 
-print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]))
-print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
+    # if the list is 1 name long, return that one name
+    if len(names) == 1:
+        return names[0]["name"]
+    
+    # if the list is 2 names long, return the two names separated by an "&"
+    if len(names) == 2:
+        return names[0]["name"] + " & " + names[0]["name"]
+
+    # Create an empty array to hold the answers
+    ans = []
+
+    # store all names into the empty array
+    for i in names:
+        ans.append(i["name"])
+
+
+# print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]))
+# print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
 print(namelist([{'name': 'Bart'},{'name': 'Lisa'}]))
 print(namelist([{'name': 'Bart'}]))
 print(namelist([]))
