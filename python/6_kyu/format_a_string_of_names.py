1349fe98c4870adb667ed40a2a00592c1424c439
gianmillare <gianmillaregsm@gmail.com>
2020-12-27 18:34:37 -0800

    completed python 6kyu format a string of names

--- python/6_kyu/format_a_string_of_names.py
+++ python/6_kyu/format_a_string_of_names.py
@@ -12,18 +12,23 @@ def namelist(names):
     
     # if the list is 2 names long, return the two names separated by an "&"
     if len(names) == 2:
-        return names[0]["name"] + " & " + names[0]["name"]
+        return names[0]["name"] + " & " + names[1]["name"]
 
     # Create an empty array to hold the answers
     ans = []
 
-    # store all names into the empty array
-    for i in names:
-        ans.append(i["name"])
+    # if the list is 3 or more names long, append a comma at the end of every name, excluding last 2
+    for i in range(0, len(names) - 2):
+        ans.append(names[i]["name"] + ",")
+    
+    # the last two names of the list will be one string combined with an ampersand
+    ans.append(names[-2]["name"] + " & " + names[-1]["name"])
+    
+    return " ".join(ans)
 
 
-# print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]))
-# print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
+print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}]))
+print(namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'}]))
 print(namelist([{'name': 'Bart'},{'name': 'Lisa'}]))
 print(namelist([{'name': 'Bart'}]))
 print(namelist([]))
