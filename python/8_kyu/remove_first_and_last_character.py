9646296471831fefe154d152216361bed08ab86c
gianmillare <gianmillaregsm@gmail.com>
2021-01-03 19:23:06 -0800

    restarted codewars repo, starting with 8kyu python remove first and last character

---
+++ python/8_kyu/remove_first_and_last_character.py
@@ -0,0 +1,9 @@
+# Remove First and Last Character
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
+
+def remove_char(s):
+    return s
+
+print(remove_char("eloquent"))
+print(remove_char("country"))
+print(remove_char("person"))
