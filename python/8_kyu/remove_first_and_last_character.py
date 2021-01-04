60bc3dda021336b05b13c7f86a12e9d683e09434
gianmillare <gianmillaregsm@gmail.com>
2021-01-03 19:24:54 -0800

    completed python 8kyu remove first and last character

--- python/8_kyu/remove_first_and_last_character.py
+++ python/8_kyu/remove_first_and_last_character.py
@@ -2,7 +2,9 @@
 # https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
 
 def remove_char(s):
-    return s
+    s = list(s)
+
+    return "".join(s[1:len(s) - 1])
 
 print(remove_char("eloquent"))
 print(remove_char("country"))
