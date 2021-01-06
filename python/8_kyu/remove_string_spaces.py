631f1ce5619c615016fbed77aa1e5c60213ddefc
gianmillare <gianmillaregsm@gmail.com>
2021-01-05 16:56:51 -0800

    completed python 8kyu remove string spaces

---
+++ python/8_kyu/remove_string_spaces.py
@@ -0,0 +1,11 @@
+# Remove String Spaces
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+def no_space(s):
+    return s.replace(" ", "")
+
+            
+
+print(no_space("8 j 8   mBliB8g  imjB8B8  jl  B"))
+print(no_space("8aaaaa dddd r     "))
+print(no_space("8j aam"))
