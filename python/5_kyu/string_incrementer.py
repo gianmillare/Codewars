9f1d1fd7b8bd1fd5f7975d7789d7588235820517
gianmillare <gianmillaregsm@gmail.com>
2021-01-08 17:36:10 -0800

    reattempting py 5kyu string incrementor to solve last challenge

--- python/5_kyu/string_incrementer.py
+++ python/5_kyu/string_incrementer.py
@@ -6,46 +6,60 @@
 # 2. if the string does not end with a number, append 1 and return the new string
 # 3. if the string ends with a number, incrememt the number by 1
 
-def incrememt_string(s):
+def increment_string(s):
     
     # Rule 1
     if len(s) == 0:
-        return 1
+        return "1"
     
     # Rule 2
     s = list(s)
-    print(s)
     if s[-1].isalpha():
         s.append("1")
         return "".join(s)
     
     # Rule 3
-    ans = []
-    for i in range(len(s)):
-        # append letters
-        if s[i].isalpha():
-            ans.append(s[i])
+    reversal = [i for i in reversed(s)]
+    ans, ind = [], 0
 
-        elif s[i].isdigit():
-            # gather the information of the numeric values, add 1 to the number
-            values_length = len(s[i:])
-            values = int("".join(s[i:])) + 1
-
-            # change the int into a list of its digits
-            value_back = list(str(values))
-
-            # if the length of the values array does not match values_length, append 0 to the front until it does
-            while values_length > len(value_back):
-                value_back.insert(0, "0")
-            
-            # append the new array into answer array
-            ans.append("".join(value_back))
+    # reverse the string, and create find the index where the integers start
+    for i in range(len(reversal)):
+        if reversal[i].isdigit():
+            ind += 1
+        else:
+            break
     
-            return "".join(ans)
+    start_inc = len(s) - ind
 
 
-# print(incrememt_string("foo"))
-# print(incrememt_string("foobar001"))
-print(incrememt_string("foobar99"))
-# print(incrememt_string("foobar099"))
-# print(incrememt_string(""))
+
+
+
+        # # append letters
+        # if s[i].isalpha():
+        #     ans.append(s[i])
+
+        # elif s[i].isdigit():
+        #     # gather the information of the numeric values, add 1 to the number
+        #     values_length = len(s[i:])
+        #     values = int("".join(s[i:])) + 1
+
+        #     # change the int into a list of its digits
+        #     value_back = list(str(values))
+
+        #     # if the length of the values array does not match values_length, append 0 to the front until it does
+        #     while values_length > len(value_back):
+        #         value_back.insert(0, "0")
+            
+        #     # append the new array into answer array
+        #     ans.append("".join(value_back))
+    
+        #     return "".join(ans)
+
+
+# print(increment_string("foo"))
+# print(increment_string("foobar001"))
+# print(increment_string("foobar99"))
+# print(increment_string("foobar099"))
+# print(increment_string(""))
+print(increment_string("1s]X>016507.Wiw=T3y67581487bNqji_*C8190579O RA7663Z2Qi~O896nU~/489027691861674357bcT304345054"))
