0d597edbbd22b0d7b767fe7d6b5a38e56a250246
gianmillare <gianmillaregsm@gmail.com>
2021-01-09 09:42:08 -0800

    created the reversed array and starting index for js 5kyu string incrementer

--- javascript/5_kyu/string_incrementer.js
+++ javascript/5_kyu/string_incrementer.js
@@ -12,12 +12,32 @@ function incrementString(s) {
     s = s.split("");
     if (s[s.length - 1].match(/[a-zA-Z]+/g)) {
         s.push("1");
-        return s;
+        return s.join("");
     }
+
+    // if the last value is a number
+    // first reverse the string array
+    reversed_array = s.reverse();
+
+    // create arrays to hold the answer, and increment index of numbers
+    var ans = [];
+    var ind = 0;
+
+    // loop through the array, if you find a digit, increment ind until not int is found
+    for (var i = 0; i < reversed_array.length; i++) {
+        if ( !(isNaN(reversed_array[i])) ) {
+            ind++;
+        } else {
+            break;
+        }
+    }
+
+    // create the starting index of the original array
+    var startInc = s.length - ind;
 }
 
-console.log(incrementString("foo"));
-// console.log(increment_string("foobar001"));
+// console.log(incrementString("foo"));
+console.log(incrementString("foobar001"));
 // console.log(increment_string("foobar99"));
 // console.log(increment_string("foobar099"));
 // console.log(increment_string(""));
