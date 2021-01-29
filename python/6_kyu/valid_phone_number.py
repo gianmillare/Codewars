9579310564641c9d8a1b70251091eaf838dad15c
gianmillare <gianmillaregsm@gmail.com>
2021-01-28 20:56:25 -0800

    started 6kyu valid phone number for py

---
+++ python/6_kyu/valid_phone_number.py
@@ -0,0 +1,17 @@
+# Valid Phone Number
+# https://www.codewars.com/kata/525f47c79f2f25a4db000025/train/python
+
+def validPhoneNumber(s):
+    s = list(s)
+    if len(s) == 14:
+        if s[0] == "(" and s[4] == ")" and s[5] == " " and s[9] == "-":
+            digit_index = [1, 2, 3, 6, 7, 8, 10, 11, 12, 13]
+            for i in range(len(s)):
+                if i in digit_index:
+                    
+
+
+
+print(validPhoneNumber("(123) 456-7890"))
+print(validPhoneNumber("(1111)555 2345"))
+print(validPhoneNumber("(098) 123 4567"))
